9d60347624496975a0079292ba9c5ece00778ab7
Michael <mquinter88@gmail.com>
2025-01-04 19:49:01 -0500

    implement basic routing and new placeholder components / install bootswatch for new theme

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,13 +1,15 @@
 import {Component, inject} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {AccountService} from '../services/account.service';
+import {AccountService} from '../_services/account.service';
 import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
+import {Router, RouterLink, RouterLinkActive} from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
   imports: [
-    FormsModule, BsDropdownModule
+    FormsModule, BsDropdownModule, RouterLink, RouterLinkActive
   ],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
@@ -15,18 +17,19 @@ import {BsDropdownModule} from 'ngx-bootstrap/dropdown';
 export class NavComponent {
 
   accountService = inject(AccountService);
+  private router = inject(Router)
+  private toastr = inject(ToastrService)
   model: any = {};
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
-      },
-      error: err => console.log(err)
+      next: () => this.router.navigateByUrl('/members'),
+      error: err => this.toastr.error(err.error)
     });
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
