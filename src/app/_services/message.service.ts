032fb43f4f679c058bc7dbf28b8cea4990c56675
Michael <mquinter88@gmail.com>
2025-01-13 17:29:27 -0500

    tidy up logs

--- src/app/_services/message.service.ts
+++ src/app/_services/message.service.ts
@@ -15,22 +15,14 @@ export class MessageService {
   paginatedResult = signal<PaginatedResult<Message[]> | null>(null);
 
   getMessages(pageNumber: number, pageSize: number, container: string) {
-    console.log(`getMessages called with pageNumber: ${pageNumber}, pageSize: ${pageSize}, container: ${container}`);
 
     let params = setPaginationHeaders(pageNumber, pageSize);
     params = params.append('Container', container);
-    console.log('Request parameters:', params);
 
     this.http.get<Message[]>(this.baseUrl + 'messages', { observe: 'response', params })
       .subscribe({
         next: response => {
-          console.log('API response received:', response);
-
-          console.log('Response body:', response.body);
-          console.log('Pagination header:', response.headers.get('Pagination'));
-
           setPaginatedResponse(response, this.paginatedResult);
-          console.log('Paginated result updated:', this.paginatedResult());
         },
         error: (error: HttpErrorResponse) => {
           console.error('Error occurred while fetching messages:', error);
